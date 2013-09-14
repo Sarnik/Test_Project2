508a55d88bbdd9022d1331a09261c73aa8330cff
Sarnik <shanwang21@gmail.com>
2013-09-14 00:21:04 -0700

    initial commit
    initial

---
+++ Hello_World.java
@@ -0,0 +1,7 @@
+public class Hello_World {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Some more code.");
+        System.out.println("Happy yet?");
+    }
+}
