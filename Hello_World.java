f4b835f04aaa943af18c7668c44e1acf122f58b3
Sarnik <shanwang21@gmail.com>
2013-09-14 00:26:29 -0700

    deleted code
    .java file

--- Hello_World.java
+++ Hello_World.java
@@ -2,6 +2,5 @@ public class Hello_World {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Some more code.");
-        System.out.println("Happy yet?");
     }
 }
